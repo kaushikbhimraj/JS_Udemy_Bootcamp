3a08ec917fc3d570103d597e08a86ba3c4755769
trixxupthesleeave <kaushik.bhimraj@gmail.com>
2020-10-22 16:38:41 -0400

    10.22.2020

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	// Create a map, add char and keep counting up
+	let chars = {};
+	let maxChar = "";
+	let count = 0;
+
+	for (let char of str){
+		chars[char] = chars[char] + 1 || 1;
+		
+		if (chars[char] > count){
+			count = chars[char];
+			maxChar = char;
+		}
+	}
+	return maxChar
+}
 
 module.exports = maxChar;
